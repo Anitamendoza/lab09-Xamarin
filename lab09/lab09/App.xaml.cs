c9bea0ce7a7baa45fdd3fb941fcda1eb0fb01db1
Alumno <Alumno@LAB1509-28>
2023-11-17 14:54:32 -0500

    Agregar archivos de proyecto.

---
+++ lab09/lab09/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using lab09.Services;
+using lab09.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab09
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ lab09/lab09/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using lab09.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace lab09.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
